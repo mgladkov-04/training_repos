b25009d6b7a864121f0ef8f54dbab7e9db08bca3
Michael <michael.gladkov.not.useless@gmail.com>
2024-06-28 11:52:10 +0300

    second commit

---
+++ newFile.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+
+int main()
+{
+	std::cout<<"I am newFile.cpp \n";
+
+return 0;
+}
+
---
+++ randomClass.h
@@ -0,0 +1,14 @@
+class random{
+
+private:
+	int num;
+	char[10] name;
+
+public:
+
+	random(int n, char[10] str)
+	{
+		num = n;
+		name = str;
+	}
+};
